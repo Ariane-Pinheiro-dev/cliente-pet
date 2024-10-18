b5990ba78ef53e0f61af5347b1584bd685e68416
Ariane Pinheiro <arianepinheirodev@gmail.com>
2024-10-17 21:34:22 -0300

    implementa controller

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
@@ -2,6 +2,7 @@ package br.com.petz.cliente_pet.cliente.application.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public interface ClienteAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	ClienteResponse postCliente(ClienteResquest clienteRequest);
+	ClienteResponse postCliente(@RequestBody ClienteResquest clienteRequest);
 
 }
