b86289eaede48da22e4a4012f9f8ebc8e255246d
Ariane Pinheiro <arianepinheirodev@gmail.com>
2024-11-01 21:21:56 -0300

    implementa fluxo service delete

--- src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteService.java
@@ -12,5 +12,6 @@ public interface ClienteService {
 	ClienteResponse criaCliente(ClienteResquest clienteRequest);
 	List<ClienteListResponse> buscaTodosClientes();
 	ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente);
+	void deletaClienteAtravesId(UUID idCliente);
 
 }
