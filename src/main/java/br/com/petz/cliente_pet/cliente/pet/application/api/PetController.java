15ba8b85af77d641f3d6f71ab19b9fa3ecf0ff11
Ariane Pinheiro <arianepinheirodev@gmail.com>
2024-11-05 22:59:31 -0300

    implementa fluxo da API

--- src/main/java/br/com/petz/cliente_pet/cliente/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/pet/application/api/PetController.java
@@ -1,22 +1,35 @@
 package br.com.petz.cliente_pet.cliente.pet.application.api;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.petz.cliente_pet.cliente.pet.application.service.PetService;
+import br.com.petz.cliente_pet.cliente.pet.domain.Pet;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class PetController implements PetApi {
-
+	private final PetService petService;
 	@Override
 	public PetResponse postPet(UUID idCliente, @Valid PetResquest petRequest) {
 		log.info("[inicia] PetController - postPet");
 		log.info("[idCliente] {}", idCliente);
+		PetResponse pet = petService.criaPet(idCliente,petRequest);
 		log.info("[finaliza] PetController - postPet");
+		return pet;
+	}
+	@Override
+	public List<PetClienteListResponse> getPetsDoClienteComId(UUID idCliente) {
+		log.info("[inicia] PetController - getPetsDoClienteComId");
+		log.info("[idCliente] {}", idCliente);
+		log.info("[finish] PetController - getPetsDoClienteComId");
 		return null;
 	}
 
