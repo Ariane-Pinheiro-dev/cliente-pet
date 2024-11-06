a1dbe91aab0bcc3db3ab35bcdb497fcb4f115d15
Ariane Pinheiro <arianepinheirodev@gmail.com>
2024-11-05 23:12:06 -0300

    implementa o fluxo do pet service busca pets do cliente

--- src/main/java/br/com/petz/cliente_pet/cliente/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/pet/application/service/PetApplicationService.java
@@ -1,10 +1,12 @@
 package br.com.petz.cliente_pet.cliente.pet.application.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import br.com.petz.cliente_pet.cliente.application.service.ClienteService;
+import br.com.petz.cliente_pet.cliente.pet.application.api.PetClienteListResponse;
 import br.com.petz.cliente_pet.cliente.pet.application.api.PetResponse;
 import br.com.petz.cliente_pet.cliente.pet.application.api.PetResquest;
 import br.com.petz.cliente_pet.cliente.pet.domain.Pet;
@@ -25,6 +27,13 @@ public class PetApplicationService implements PetService {
 		Pet pet = petRepository.salvaPet(new Pet(idCliente, petRequest));
 		log.info("[finish] PetApplicationService - criaPet");
 		return new PetResponse(pet.getIdPet());
+	}
+	@Override
+	public List<PetClienteListResponse> buscaPetsDoClienteComID(UUID idCliente) {
+		log.info("[start] PetApplicationService - buscaPetsDoClienteComID");
+		clienteService.buscaClienteAtravesId(idCliente);
+		log.info("[finish] PetApplicationService - buscaPetsDoClienteComID");
+		return null;
 	}
 
 }
--- src/main/java/br/com/petz/cliente_pet/cliente/pet/application/service/PetService.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/pet/application/service/PetService.java
@@ -1,12 +1,16 @@
 package br.com.petz.cliente_pet.cliente.pet.application.service;
 
+import java.util.List;
 import java.util.UUID;
 
+import br.com.petz.cliente_pet.cliente.pet.application.api.PetClienteListResponse;
 import br.com.petz.cliente_pet.cliente.pet.application.api.PetResponse;
 import br.com.petz.cliente_pet.cliente.pet.application.api.PetResquest;
 import jakarta.validation.Valid;
 
 public interface PetService {
 	PetResponse criaPet(UUID idCliente, @Valid PetResquest petRequest);
+
+	List<PetClienteListResponse> buscaPetsDoClienteComID(UUID idCliente);
 
 }
